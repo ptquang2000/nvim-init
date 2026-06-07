4c61a3335ec0c589ac28304b3dad62a782c0d7c6
quang.phan <ptquang2000@gmail.com>
2026-06-07 12:33:26 +0700

    upd telescope kb

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,10 +5,8 @@ require("custom.lsp")
 require("custom.telescope")
 require("custom.conform")
 require("custom.treesitter")
-require("custom.markview")
 require("custom.windows")
 require("custom.harpoon")
-require("custom.vimtex")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("HighlightYank", {}),
