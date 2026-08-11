bd04b28c250e0321e0adb275bb03eee847c84ab9
quang.phan <ptquang2000@gmail.com>
2026-08-12 06:19:42 +0700

    upd dap

--- lua/custom/dap.lua
+++ lua/custom/dap.lua
@@ -33,33 +33,31 @@ dapui.setup({
 	layouts = {
 		{
 			elements = {
-				{ id = "scopes", size = 0.33 },
-				{ id = "watches", size = 0.33 },
-				{ id = "breakpoints", size = 0.33 },
+				{ id = "scopes", size = 0.5 },
+				{ id = "watches", size = 0.5 },
 			},
 			size = 0.33,
-			position = "left",
+			position = "bottom",
 		},
 		{
 			elements = {
-				{ id = "repl", size = 0.5 },
-				{ id = "stacks", size = 0.5 },
+				{ id = "stacks", size = 0.7 },
+				{ id = "repl", size = 0.3 },
 			},
-			size = 0.25,
-			position = "bottom",
+			size = 0.5,
+			position = "right",
 		},
 	},
 	controls = {
 		element = "repl",
 		icons = {
-			pause = "󰏤",
-			play = "󰐊",
-			step_into = "󰆹",
-			step_over = "󰆺",
-			step_out = "󰆻",
-			step_back = "󰆸",
-			run_last = "󰑒",
-			terminate = "󰧧",
+			pause = "",
+			play = "",
+			step_over = "",
+			step_into = "",
+			step_out = "",
+			run_last = "",
+			terminate = "",
 		},
 	},
 })
@@ -80,13 +78,12 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
-
 vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
+vim.keymap.set("n", "<F17>", dap.terminate, { desc = "DAP: Terminate <S-F5>" })
 vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "DAP: Breakpoint" })
+vim.keymap.set("n", "<F21>", function()
+	dap.set_breakpoint(vim.fn.input("Condition: "))
+end, { desc = "DAP: Breakpoint (condition) <S-F9>" })
 vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step over" })
 vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step into" })
-vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step out" })
-vim.keymap.set("n", "<S-F5>", dap.terminate, { desc = "DAP: Terminate" })
-vim.keymap.set("n", "<S-F9>", function()
-	dap.set_breakpoint(vim.fn.input("Condition: "))
-end, { desc = "DAP: Breakpoint (condition)" })
+vim.keymap.set("n", "<F23>", dap.step_out, { desc = "DAP: Step out <S-F11>" })
