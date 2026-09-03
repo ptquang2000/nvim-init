a3c413cc700facf9a92718238750976c288d9e7d
quang.phan <ptquang2000@gmail.com>
2026-09-03 10:04:39 +0700

    fix window

--- lua/custom/windows.lua
+++ lua/custom/windows.lua
@@ -27,7 +27,7 @@ vim.lsp.config["clangd"] = {
 		"force",
 		{},
 		vim.lsp.protocol.make_client_capabilities(),
-		require("cmp_nvim_lsp").default_capabilities()
+		require("blink.cmp").get_lsp_capabilities()
 	),
 }
 
